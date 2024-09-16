d623bb0c97fe02be7eb945575227f0e65c8e7366
Jyoth <77140785+jyothbhoga@users.noreply.github.com>
2024-09-16 13:18:00 +0530

    fix: env fix

--- src/routes/vehicles.js
+++ src/routes/vehicles.js
@@ -1,5 +1,9 @@
 import dotenv from "dotenv";
-dotenv.config();
+import path from "path";
+
+const envFile =
+  process.env.NODE_ENV === "development" ? ".env.dev" : ".env.prod";
+dotenv.config({ path: path.resolve(process.cwd(), envFile) });
 
 import express from "express";
 import Vehicle from "../models/vehicle.js";
